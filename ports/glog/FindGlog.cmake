6220e7f19bbed49fbe8b568e6ae47eee356460e3
Michael Menefee <mikem@mnetwork.org>
2018-04-04 18:57:04 -0500

    [glog] Adding FindGlog.cmake support

---
+++ ports/glog/FindGlog.cmake
@@ -0,0 +1,2 @@
+include(${CMAKE_CURRENT_LIST_DIR}/glog-config.cmake)
+set(Glog_FOUND TRUE)
--- ports/glog/portfile.cmake
+++ ports/glog/portfile.cmake
@@ -29,6 +29,8 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 vcpkg_copy_pdbs()
 
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/FindGlog.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/glog)
+
 # Handle copyright
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/glog)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/glog/COPYING ${CURRENT_PACKAGES_DIR}/share/glog/copyright)
