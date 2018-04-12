063adba1d5b47966465bfde1d89d16ecc1ce8467
Michael Menefee <mikem@mnetwork.org>
2018-04-12 00:22:43 -0500

    [tools] fixing git path & removing MKL dist

--- set_dev_path.bat
+++ set_dev_path.bat
@@ -6,11 +6,9 @@ set OLD_PATH=%Path%
 set Path=%VCPKG_PATH%installed\x64-windows\bin\
 set Path=%Path%;%VCPKG_PATH%installed\x64-windows\lib\
 set Path=%Path%;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin\
-set Path=%Path%;C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\redist\intel64\mkl\
 set Path=%Path%;C:\gstreamer\1.0\x86_64\bin\
-set Path=%Path%;C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2018\windows\redist\intel64\mkl\
 set Path=%Path%;%VCPKG_PATH%downloads\cmake-3.11.0-win32-x86\bin\
-set Path=%Path%;%VCPKG_PATH%downloads\MinGit-2.17.0-32\cmd\
+set Path=%Path%;%VCPKG_PATH%downloads\MinGit-2.17.0-32-bit\cmd\
 
 set Path=%Path%;%OLD_PATH%
 
