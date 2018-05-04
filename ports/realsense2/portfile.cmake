ee02680cd857a579efbf095269bd539d647a7531
Michael Menefee <mikem@mnetwork.org>
2018-05-04 00:23:47 -0500

    [realsense2] update to latest version, add wrapper support

--- ports/realsense2/portfile.cmake
+++ ports/realsense2/portfile.cmake
@@ -1,42 +1,62 @@
 include(vcpkg_common_functions)
 
+set(REALSENSE_VERSION v2.10.4)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO IntelRealSense/librealsense
-    REF v2.10.1
-    SHA512 fb00a424a5bd7335cc661261e76cf623e27a89af1033692d4cb6ed523af1295359929c235e82253911e61323cb7b82551a9223862174cb0e2363ac944b2db923
-    HEAD_REF development
+    REF ${REALSENSE_VERSION}
+    SHA512 35580cd4ab65b85eb7fcebac3be629960993223437e3c44b0bcc2f7572d85231e822a922b2f5e22480fcc1edb9295ab2c5893794d638c2ab6faf49a9eea57603
+    HEAD_REF master
 )
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/build_with_static_crt.patch # https://github.com/IntelRealSense/librealsense/pull/1262
-        ${CMAKE_CURRENT_LIST_DIR}/fix_rgb_using_avx2.patch # https://github.com/IntelRealSense/librealsense/pull/1245
-)
+# vcpkg_apply_patches(
+#     SOURCE_PATH ${SOURCE_PATH}
+#     PATCHES
+# )
 
-# This option will be deprecated in the later versions.
-# Please see Pull Request #1245. https://github.com/IntelRealSense/librealsense/pull/1245
-set(RGB_USING_AVX2 OFF)
-if("avx2" IN_LIST FEATURES)
-    set(RGB_USING_AVX2 ON)
+set(CMAKE_MODULE_PATH)
+
+set(BUILD_EXAMPLES OFF)
+if("examples" IN_LIST FEATURES)
+    set(BUILD_EXAMPLES ON)
+    set(BUILD_GRAPHICAL_EXAMPLES ON)
+endif()
+
+# set(BUILD_GRAPHICAL_EXAMPLES OFF)
+# if("graphical-examples" IN_LIST FEATURES)
+#     set(BUILD_GRAPHICAL_EXAMPLES ON)
+# endif()
+
+set(BUILD_PYTHON_BINDINGS OFF)
+if("python3" IN_LIST FEATURES)
+    set(BUILD_PYTHON_BINDINGS ON)
+    list(APPEND CMAKE_MODULE_PATH ${CURRENT_INSTALLED_DIR}/share/python3)
+endif()
+
+set(BUILD_CV_EXAMPLESS OFF)
+if("opencv" IN_LIST FEATURES)
+    set(BUILD_CV_EXAMPLES ON)
+    list(APPEND CMAKE_MODULE_PATH ${CURRENT_INSTALLED_DIR}/share/opencv)
 endif()
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_LIBRARY_LINKAGE)
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" BUILD_CRT_LINKAGE)
 
+string(REPLACE ";" "\\\\\;" CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}")
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DENFORCE_METADATA=ON
-        -DBUILD_EXAMPLES=OFF
-        -DBUILD_GRAPHICAL_EXAMPLES=OFF
-        -DBUILD_PYTHON_BINDINGS=OFF
+        -DBUILD_EXAMPLES=${BUILD_EXAMPLES}
+        -DBUILD_GRAPHICAL_EXAMPLES=${BUILD_GRAPHICAL_EXAMPLES}
+        -DBUILD_PYTHON_BINDINGS=${BUILD_PYTHON_BINDINGS}
         -DBUILD_UNIT_TESTS=OFF
         -DBUILD_WITH_OPENMP=OFF
         -DBUILD_SHARED_LIBS=${BUILD_LIBRARY_LINKAGE}
         -DBUILD_WITH_STATIC_CRT=${BUILD_CRT_LINKAGE}
-        -DRGB_USING_AVX2=${RGB_USING_AVX2}
+        "-DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}"
     OPTIONS_DEBUG
         "-DCMAKE_PDB_OUTPUT_DIRECTORY=${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg"
         -DCMAKE_DEBUG_POSTFIX="_d"
@@ -46,7 +66,19 @@ vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/realsense2)
 
+if(BUILD_EXAMPLES)
+    file(GLOB EXE_FILES ${CURRENT_PACKAGES_DIR}/bin/*.exe)
+    file(GLOB DEBUG_EXE_FILES ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
+
+    file(INSTALL ${EXE_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/realsense2)
+    file(INSTALL ${DEBUG_EXE_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools/realsense2)
+
+    file(REMOVE ${EXE_FILES})
+    file(REMOVE ${DEBUG_EXE_FILES})
+endif()
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
 # Handle copyright
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/realsense2)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/realsense2/COPYING ${CURRENT_PACKAGES_DIR}/share/realsense2/copyright)
